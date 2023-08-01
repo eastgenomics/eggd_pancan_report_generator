824c580eb869120d22571ad6cc18a97545721901
AishaD <AishaDahir75@gmail.com>
2023-08-01 18:16:51 +0100

    PCF-127 clarify comments

--- resources/home/dnanexus/chimeraviz.R
+++ resources/home/dnanexus/chimeraviz.R
@@ -13,23 +13,23 @@ suppressMessages(library(chimeraviz))
 # args 4 = fusion_number
 
 
-# get ensb ann
+# get ensb annotation sqlite input from cmd line
 edb <- ensembldb::EnsDb(args[1])
-# get bam file
+# get bam file input from cmd line
 fusion_reads <- args[2]
-# get one fusion from list of fusions
+# get fusion number input from cmd line
 FI_fusions <- import_starfusion(args[3], "hg38")
 fusion <- get_fusion_by_id(FI_fusions, args[4]) 
 fusion_df <- read.csv(args[3], sep = "\t")
 filename <- fusion_df[args[4],1]
 
-## plots
+## plot 1 - overall gene fusion
 png(paste(filename, "gene_plot.png", sep = "_"))
 plot_fusion_transcript(
     fusion,
     edb)
 dev.off()
-
+## plot 2 - per transcript with coverage
 png(paste(filename, "transcript_plot.png", sep = "_"), width = 1000)
 plot_fusion(
     fusion = fusion,
